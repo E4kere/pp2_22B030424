7d5ace6ceb382d76a1e8d1d4f2360b0bb38680a3
E4kere <sagataly18@gmail.com>
2023-02-01 23:55:57 +0600

    j

---
+++ TSIS2/b.py
@@ -0,0 +1,14 @@
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+  print(" ")
+
+
+  adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for k in adj:
+  for y in fruits:
+    print(k, y)
